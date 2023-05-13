c72712e2fe5d5272ec5380168dcb2409def71bee
POSI UNS 2020 <123924893+posiuns2020@users.noreply.github.com>
2023-05-13 19:15:19 +0700

    Modul 08

---
+++ Geometri2D.py
@@ -0,0 +1,35 @@
+# MEMBUAT MODUL #
+
+import math
+
+# Fungsi Luas Persegi Panjang
+def LuasPersegiPanjang(p,l):
+    return p*l
+
+# Fungsi Keliling Persegi Panjang
+def KelilingPersegiPanjang(p,l):
+    return 2*(p+l)
+
+# Fungsi Luas Bujur Sangkar
+def LuasBujurSangkar(s):
+    return s*s
+
+# Fungsi Keliling Bujur Sangkar
+def KelilingBujurSangkar(s):
+    return 4*s
+
+# Fungsi Luas Lingkaran
+def LuasLingkaran(r):
+    return math.pi*r*r
+
+# Fungsi Keliling Lingkaran
+def KelilingLingkaran(r):
+    return 2*math.pi*r
+
+# Fungsi Luas Segitiga
+def LuasSegitiga(a,t):
+    return (a*t)/2
+
+# Fungsi Keliling Segitiga
+def KelilingSegitiga(a,b,c):
+    return a+b+c
